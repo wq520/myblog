b7a260c475eb920fa9aecfedcb82d718543dfc10
wq520 <1353257412@qq.com>
2018-09-03 18:35:21 +0800

    add new

--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,9 @@
 const Router = require('koa-router')
+// 拿到操作user表的逻辑对象
+const user = require('../control/user')
 const router = new Router
 
-//设置主页
+// 设置主页
 router.get("/", async (ctx) => {
     await ctx.render("index", {
         title: "寒光博客",
@@ -11,11 +13,17 @@ router.get("/", async (ctx) => {
     })
 })
 
-//主要用来处理 用户登录 注册
+// 主要用来处理 用户登录 注册
 router.get(/^\/user\/(?=reg|login)/, async (ctx) => {
     // show 为 true 显示注册否则显示登录
     const show = /reg$/.test(ctx.path)
     await ctx.render("register", {show})
 })
+
+// 注册用户 路由
+router.post("/user/reg", user.reg)
+
+// 用户登录 路由
+router.post("/user/login", user.login)
 
 module.exports = router
